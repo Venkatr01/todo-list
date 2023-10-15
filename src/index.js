736e6c8fbfcc92266e0f29dab6e6f0af1b5a2735
Venkat <vraju4865@gmail.com>
2023-10-15 12:51:53 +0530

    Remove todo modules and delete code

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import todos from "./todos";
-import todoForm from "./todoForm";
-
-const addtodoBtn = document.querySelector('#addtodoBtn');
-addtodoBtn.addEventListener('click',()=>{
-    todos();
-    todoForm();
-})
